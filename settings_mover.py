fe7a4fba488ad71d96cf53168d85c33340869158
Randy Lai <randy.cs.lai@gmail.com>
2019-10-24 14:04:12 -0700

    improve settings mover

--- settings_mover.py
+++ settings_mover.py
@@ -4,22 +4,22 @@ import os
 
 
 def update_settings():
-    keys = [
-            "git",
-            "token",
-            "refresh",
-            "cooldown",
-            "ignore_services",
-            "debug"
-           ]
+    default_settings = {
+            "git": "git",
+            "token": {},
+            "refresh": 30,
+            "cooldown": 60,
+            "ignore_services": ["github/pages"],
+            "debug": False
+    }
 
     s = sublime.load_settings("GitHubBuildStatus.sublime-settings")
     t = sublime.load_settings("github_checks.sublime-settings")
 
-    for k in keys:
+    for k in default_settings.keys():
         svalue = s.get(k)
         tvalue = t.get(k)
-        if svalue and tvalue is None:
+        if svalue and svalue != default_settings[k] and tvalue == default_settings[k]:
             t.set(k, svalue)
             sublime.save_settings("github_checks.sublime-settings")
 
@@ -30,5 +30,6 @@ def update_settings():
 
 def plugin_loaded():
     f = os.path.join(sublime.packages_path(), "User", "GitHubBuildStatus.sublime-settings")
-    if os.path.exists(f):
+    g = os.path.join(sublime.packages_path(), "User", "github_checks.sublime-settings")
+    if os.path.exists(f) and not os.path.exists(g):
         update_settings()
